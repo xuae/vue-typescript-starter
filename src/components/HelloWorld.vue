007797cbf11d701d108d229fceca757346603bd8
xuae <55976226+xuae@users.noreply.github.com>
2019-12-15 21:06:39 +0800

    添加mixin混入、组件参数、计算属性、方法的写法。

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,59 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ title }}</h1>
+    <h2>message: {{ message }}</h2>
+    <h2>mixinProp: {{ mixinProp }}</h2>
+    <h2>computed msg: {{ msg }}</h2>
   </div>
 </template>
 
 <script lang="ts">
-  import { Component, Prop, Vue } from 'vue-property-decorator';
+  import { Component, Mixins, Prop } from 'vue-property-decorator';
+  import MixinModule from '@/mixins/MixinModule';
+
+  type TMessage = 'Hello' | 'hello';
+  const messages = ['Hello', 'hello'];
 
   @Component
-  export default class HelloWorld extends Vue {
-    @Prop() private msg!: string;
+  export default class HelloWorld extends Mixins(MixinModule) {
+    @Prop({
+      type: String as () => TMessage, // 不能省略 as () =>
+      validator(value: any): boolean {
+        return messages.includes(value);
+      },
+    })
+    private message?: TMessage;
+
+    @Prop({
+      type: String, // 首字母大写，遵循的是Vue prop的参数类型
+      required: true,
+    })
+    title!: string; // typescript的类型
+
+    // computed 计算属性
+    get msg(): string {
+      return `${this.message} ${this.mixinProp}`;
+    }
+
+    // method
+    helloWorldMethod(): void {
+      console.log(this.mixinMethod());
+      console.log(`Hello World Component title is "${this.title}"`);
+    }
+
+    // 生命周期
+    beforeCreated() {
+      //
+    }
+    created() {
+      //
+    }
+    beforeMounted() {
+      //
+    }
+    mounted() {
+      //
+    }
   }
 </script>
 
